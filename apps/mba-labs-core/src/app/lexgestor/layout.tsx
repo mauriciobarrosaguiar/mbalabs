5dcfad7bc6d2ae390e55c9ae0330707fa13d2458
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-06-18 08:08:00 -0300

    Importa ajustes mobile do LexGestor

--- apps/mba-labs-core/src/app/lexgestor/layout.tsx
+++ apps/mba-labs-core/src/app/lexgestor/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import "./lexgestor.css";
 import "./lexgestor-documents.css";
+import "./lexgestor-mobile.css";
 import { HeaderLexGestor } from "@/components/lexgestor/HeaderLexGestor";
 import { MobileNavLexGestor } from "@/components/lexgestor/MobileNavLexGestor";
 import { SidebarLexGestor } from "@/components/lexgestor/SidebarLexGestor";
