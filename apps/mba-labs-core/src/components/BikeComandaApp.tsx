04889b3ef27625c8c9fecb5b5ab624eec0cc205a
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-06-28 16:12:24 -0300

    chore: atualizar cache do BikeComanda para menu estilo LavaGestor

--- apps/mba-labs-core/src/components/BikeComandaApp.tsx
+++ apps/mba-labs-core/src/components/BikeComandaApp.tsx
@@ -96,7 +96,7 @@ export function BikeComandaApp({ activeSlug = "dashboard" }: BikeComandaAppProps
       }}
     >
       <iframe
-        src={`/bikecomanda-static/index.html?view=${encodeURIComponent(initialView)}&v=20260628-menu-mobile`}
+        src={`/bikecomanda-static/index.html?view=${encodeURIComponent(initialView)}&v=20260628-lava-menu-style`}
         title="BikeComanda"
         style={{
           display: "block",
