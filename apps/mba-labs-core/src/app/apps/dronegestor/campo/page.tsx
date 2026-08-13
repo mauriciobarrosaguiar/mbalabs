3f966f2f0afa12a68c1b88de615f25a62e8a64ea
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-08-13 09:30:42 -0300

    DroneGestor: conciliar central de liberação com dica de cálculo

--- apps/mba-labs-core/src/app/apps/dronegestor/campo/page.tsx
+++ apps/mba-labs-core/src/app/apps/dronegestor/campo/page.tsx
@@ -2,6 +2,7 @@ import { requireAppAccess } from "@/lib/core-data";
 import { DroneEquipmentPicker } from "./DroneEquipmentPicker";
 import { DroneGestorAppV3 } from "./DroneGestorAppV3";
 import { DroneMissionContextReset } from "./DroneMissionContextReset";
+import { DroneMissionGateHint } from "./DroneMissionGateHint";
 import { DroneOsLifecycleSync } from "./DroneOsLifecycleSync";
 import { DronePersistenceSync } from "./DronePersistenceSync";
 import { DroneProductMissionPicker } from "./DroneProductMissionPicker";
@@ -29,6 +30,7 @@ export default async function DroneGestorCampoPage() {
       <DroneResolveRoute />
       <DroneEquipmentPicker canManage={canManage} />
       <DroneSimpleFlowUX />
+      <DroneMissionGateHint />
       <DroneRegulatoryGuard />
       <DroneProductMissionPicker />
       <DroneSarpasStateBridge />
