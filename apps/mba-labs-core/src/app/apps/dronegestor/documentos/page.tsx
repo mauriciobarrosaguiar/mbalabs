850f83ceaece5240001c01c9c0ad3a59f49cbad2
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-08-14 21:38:40 -0300

    DroneGestor: aplica regra SARPAS agrícola na central de documentos

--- apps/mba-labs-core/src/app/apps/dronegestor/documentos/page.tsx
+++ apps/mba-labs-core/src/app/apps/dronegestor/documentos/page.tsx
@@ -1,9 +1,13 @@
 import { requireAppAccess } from "@/lib/core-data";
+import { DroneSarpasAgriculturalGuard } from "../DroneSarpasAgriculturalGuard";
 import { DroneDocumentsCenter } from "./DroneDocumentsCenter";
 
 export const dynamic = "force-dynamic";
 
 export default async function DroneDocumentsPage() {
   await requireAppAccess("dronegestor", "/apps/dronegestor/documentos");
-  return <DroneDocumentsCenter />;
+  return <>
+    <DroneSarpasAgriculturalGuard />
+    <DroneDocumentsCenter />
+  </>;
 }
