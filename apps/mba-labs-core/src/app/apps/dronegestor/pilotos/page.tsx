8b9b78fb3fe8e42f1e2ca88c81c44ead2f5a5e08
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-08-14 21:49:40 -0300

    DroneGestor: exibe qualificação junto aos pilotos

--- apps/mba-labs-core/src/app/apps/dronegestor/pilotos/page.tsx
+++ apps/mba-labs-core/src/app/apps/dronegestor/pilotos/page.tsx
@@ -1,9 +1,13 @@
 import { requireAppAccess } from "@/lib/core-data";
+import { DronePilotQualifications } from "./DronePilotQualifications";
 import { DronePilotsManager } from "./DronePilotsManager";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page(){
   await requireAppAccess("dronegestor", "/apps/dronegestor/pilotos");
-  return <DronePilotsManager/>;
+  return <>
+    <DronePilotQualifications />
+    <DronePilotsManager/>
+  </>;
 }
