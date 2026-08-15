bf29779ef7316ad5a59ea38e898e884c12ae1030
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-08-15 06:37:29 -0300

    DroneGestor: monta resumo final no pacote da operação

--- apps/mba-labs-core/src/app/apps/dronegestor/pacote-operacao/page.tsx
+++ apps/mba-labs-core/src/app/apps/dronegestor/pacote-operacao/page.tsx
@@ -1,9 +1,10 @@
 import { requireAppAccess } from "@/lib/core-data";
+import { DroneFinalOsSummary } from "./DroneFinalOsSummary";
 import { DroneOperationPackage } from "./DroneOperationPackage";
 
 export const dynamic = "force-dynamic";
 
 export default async function DroneOperationPackagePage() {
   await requireAppAccess("dronegestor", "/apps/dronegestor/pacote-operacao");
-  return <DroneOperationPackage />;
+  return <><DroneFinalOsSummary/><DroneOperationPackage/></>;
 }
