ec711ad0c736fab3bd0516edf19cf6421aa6692b
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-09-03 06:52:32 -0700

    Remove home pública do Elshaday

--- apps/mba-labs-core/src/app/elshaday/page.tsx
+++ apps/mba-labs-core/src/app/elshaday/page.tsx
@@ -1,15 +1,7 @@
-import { getOptionalElshadayContext } from "@/lib/elshaday";
 import { PrivateElshadayDashboard } from "./PrivateElshadayDashboard";
-import { PublicElshadayHome } from "./PublicElshadayHome";
 
 export const dynamic = "force-dynamic";
 
 export default async function ElshadayHomePage() {
-  const context = await getOptionalElshadayContext();
-
-  if (context) {
-    return <PrivateElshadayDashboard />;
-  }
-
-  return <PublicElshadayHome showMembership />;
+  return <PrivateElshadayDashboard />;
 }
