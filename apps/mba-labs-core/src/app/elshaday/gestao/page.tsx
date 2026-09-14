4a1a708a3f7dc2f653bad3fb318b3aff35bfd060
mauriciobarrosaguiar <emsgenericosto@gmail.com>
2026-09-14 08:44:44 -0300

    feat(elshaday): integrar galeria a home

--- apps/mba-labs-core/src/app/elshaday/gestao/page.tsx
+++ apps/mba-labs-core/src/app/elshaday/gestao/page.tsx
@@ -1,7 +1,13 @@
 import { PrivateElshadayDashboard } from "../PrivateElshadayDashboard";
+import { RecentGalleryAlbums } from "../RecentGalleryAlbums";
 
 export const dynamic = "force-dynamic";
 
 export default async function ElshadayManagementHomePage() {
-  return <PrivateElshadayDashboard />;
+  return (
+    <>
+      <PrivateElshadayDashboard />
+      <RecentGalleryAlbums />
+    </>
+  );
 }
